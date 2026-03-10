7921a3f3310b96efea1903558a2cd26035bb96a4
Saif <droconos99@gmail.com>
2026-03-10 22:40:45 +0600

    Add: View for QR Scan

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import http from "http";
 import { handleError, handleNotFound } from "./middlewares/error.middleware.js";
 import logger from "./utils/logger.js";
 import { initSocketServer } from "./sockets/socketHandler.js";
+import messageRoutes from "./routes/message.routes.js";
 import { whatsappService } from "./services/whatsapp.services.js";
 
 const app = express();
@@ -21,6 +22,8 @@ app.use(express.static(path.join(__dirname, "..", "public")));
 app.get("/server-status", (_req: Request, res: Response) => {
    res.json({ success: true, message: "Server is running", uptime: process.uptime() });
 });
+
+app.use("/api", messageRoutes);
 
 app.use(handleNotFound);
 app.use(handleError);
